6931657945f3add349fa250b0cdf3a1fca9447bd
Gabriel Tolfo <gabrieltolfo2014@gmail.com>
2025-12-26 20:15:43 -0400

    initial commit

--- install.sh
+++ install.sh
@@ -6,7 +6,13 @@ pip install -r requirements.txt
 
 chmod +x req.py
 
-cp req.py /usr/local/bin/req-html
+if [ -n "$PREFIX"]; then
+    echo "[*] Detected Termux environment."
+    cp req.py $PREFIX/bin/req-html
+else
+    echo "[*] Detected Linux"
+    sudo cp req.py /usr/local/bin/req-html
+fi
 
 echo "[+] Installed!"
 echo "[+] Use: req-html <url>"
